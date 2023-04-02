7d0821d2d19dc6fea5b0bb6e529539b26efe1b09
abishekbebito <abi712ato@gmail.com>
2023-04-02 22:37:20 +0530

    reg ex for email is added but wrong

--- login page/loginPage.js
+++ login page/loginPage.js
@@ -1,21 +1,28 @@
 function passwordValidation() {
   var password = document.getElementById("pass").value;
-  if (password == "") {
-    document.getElementById("message").innerHTML = "password empty";
-    return;
+  var email = document.getElementById("mailId").value;
+  var validEmail = /^[a-z0-9]+[@][a-z]+\.[a-z]{3}$/;
+  if (password == "" || email == "") {
+    document.getElementById("message").innerHTML =
+      "please fill the credentials";
+    return false;
+  }
+  if (email != validEmail) {
+    document.getElementById("message").innerHTML = "email incorrecct formaat";
+    return false;
   }
   if (password.length < 8) {
     document.getElementById("message").innerHTML = "should not be less than 8";
-    return;
+    return false;
   }
   if (password.length > 15) {
     document.getElementById("message").innerHTML =
       "should not be greater  than 15";
-    return;
+    return false;
   }
 
-  if (password == 123456789) {
+  if (password == 123456789 && email == "hello@gmail.com") {
     alert("password submitted");
-    return;
+    return true;
   }
 }
